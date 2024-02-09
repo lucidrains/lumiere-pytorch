f9dbc972440904adf2c3a326c7f7a76685eec8fb
lucidrains <lucidrains@gmail.com>
2024-02-09 09:03:03 -0800

    complete the two inflation modules, allow for either images all folded into batch, or video format (channel first)

--- lumiere_pytorch/lumiere_pytorch.py
+++ lumiere_pytorch/lumiere_pytorch.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn, einsum
-from torch.nn import Module
+from torch.nn import Module, ModuleList
 import torch.nn.functional as F
 
-from einops import rearrange
+from einops import rearrange, pack, unpack
 
 from x_transformers.x_transformers import (
     Attention,
@@ -17,6 +17,12 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+def pack_one(t, pattern):
+    return pack([t], pattern)
+
+def unpack_one(t, ps, pattern):
+    return unpack(t, ps, pattern)[0]
 
 def divisible_by(num, den):
     return (num % den) == 0
@@ -51,40 +57,92 @@ class ConvolutionInflationBlock(Module):
             nn.SiLU()
         )
 
-        self.proj_out = nn.Conv2d(dim, dim, 1)
+        self.proj_out = nn.Conv1d(dim, dim, 1)
 
     def forward(
         self,
-        images,
+        x,
         batch_size = None
     ):
-        return images
+        residual = x
+        is_video = x.ndim == 5
+        assert is_video ^ exists(batch_size), 'either a tensor of shape (batch, channels, time, height, width) is passed in, or (batch * time, channels, height, width) along with `batch_size`'
+
+        if is_video:
+            batch_size = x.shape[0]
+            x = rearrange(x, 'b c t h w -> (b t) c h w')
+
+        x = self.spatial_conv(x)
+
+        x = rearrange(x, '(b t) c h w -> b h w c t', b = batch_size)
+
+        x, ps = pack_one(x, '* c t')
+
+        x = self.temporal_conv(x)
+        x = self.proj_out(x)
+
+        x = unpack_one(x, ps, '* c t')
+
+        if is_video:
+            x = rearrange(x, 'b h w c t -> b c t h w')
+        else:
+            x = rearrange(x, 'b h w c t -> (b t) c h w')
+
+        return x + residual
 
 class AttentionInflationBlock(Module):
     def __init__(
         self,
         *,
         dim,
-        depth,
+        depth = 1,
         **attn_kwargs
     ):
         super().__init__()
 
-        self.attns = ModuleList([])
+        self.temporal_attns = ModuleList([])
 
         for _ in range(depth):
-            attn = Attention(
-                dim = dim,
-                **attn_kwargs
+            attn = nn.Sequential(
+                RMSNorm(dim),
+                Attention(
+                    dim = dim,
+                    **attn_kwargs
+                )
             )
 
-            self.attns.append(attn)
+            self.temporal_attns.append(attn)
 
-        self.proj_out = nn.Conv2d(dim, dim, 1)
+        self.proj_out = nn.Linear(dim, dim)
 
     def forward(
         self,
-        images,
+        x,
         batch_size = None
     ):
-        return images
+        residual = x
+        is_video = x.ndim == 5
+        assert is_video ^ exists(batch_size), 'either a tensor of shape (batch, channels, time, height, width) is passed in, or (batch * time, channels, height, width) along with `batch_size`'
+
+        if is_video:
+            batch_size = x.shape[0]
+            x = rearrange(x, 'b c t h w -> b h w t c')
+        else:
+            assert exists(batch_size)
+            x = rearrange(x, '(b t) c h w -> b h w t c', b = batch_size)
+
+        x, ps = pack_one(x, '* t c')
+
+        for attn in self.temporal_attns:
+            x = attn(x) + x
+
+        x = self.proj_out(x)
+
+        x = unpack_one(x, ps, '* t c')
+
+        if is_video:
+            x = rearrange(x, 'b h w t c -> b c t h w')
+        else:
+            x = rearrange(x, 'b h w t c -> (b t) c h w')
+
+        return x + residual
